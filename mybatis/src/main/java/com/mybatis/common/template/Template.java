d8623ad74ac4afce945f6fbd58639677169b47ec
LeeHaNeal <harull817@naver.com>
2025-03-20 18:14:45 +0900

    2025년 03월 20일

---
+++ mybatis/src/main/java/com/mybatis/common/template/Template.java
@@ -0,0 +1,31 @@
+package com.mybatis.common.template;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class Template {
+	public static SqlSession getSqlSession() {
+		SqlSession sqlSession = null;
+		
+		// SqlSession 생성하기 위해서는 sqlsessionFactory 필요
+		// sqlsessionFactory => sqlsessionFactoryBuilder
+		String resources = "/mybatis-config.xml";
+		try {
+		InputStream stream = Resources.getResourceAsStream(resources);
+		sqlSession = new SqlSessionFactoryBuilder().build(stream).openSession(false);
+		// openSession(flase) : 수동 commit => 기본값 , true => 자동커밋
+		
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		 return sqlSession;
+		
+	}
+	
+	
+	
+}
